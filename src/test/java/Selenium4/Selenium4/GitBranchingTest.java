5910ac5729e467af914f887940e9855167786ab2
codenbox <codenbox2014@gmail.com>
2020-02-04 12:02:39 -0500

    added new class name as GitBranchingTest

---
+++ src/test/java/Selenium4/Selenium4/GitBranchingTest.java
@@ -0,0 +1,13 @@
+package Selenium4.Selenium4;
+
+import org.testng.annotations.Test;
+
+public class GitBranchingTest {
+
+	@Test
+	public void demo() {
+		System.out.println("This is demo method for branching test");
+
+	}
+
+}
